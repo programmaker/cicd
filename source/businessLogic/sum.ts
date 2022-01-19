c20b192c8ca5059e01158f45d3998ef2094d5660
Ilia Alekseenko <programmaker@gmail.com>
2022-01-19 20:53:35 +1300

    added unit test

---
+++ source/businessLogic/sum.ts
@@ -0,0 +1,5 @@
+let sum: ( a: number, b: number ) => number = function(a,b) {
+    return a+b;
+}
+
+module.exports = sum;
---
+++ tests/sum.test.js
@@ -0,0 +1,8 @@
+const sum = require('../source/businessLogic/sum');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+test('adds 3 + 2 to equal 5', () => {
+    expect(sum(3, 2)).toBe(5);
+  });
